2ef01b0145b900faaca037770d88e1f157cba2ea
konata-chan404 <42537566+konata-chan404@users.noreply.github.com>
2020-10-18 12:12:43 +0300

    gif options
    yesss

--- bot.py
+++ bot.py
@@ -17,13 +17,39 @@ async def someone(ctx):
     await ctx.send(random.choice(ctx.channel.members).mention)
 
 @bot.command()
-async def shufersal(ctx, product_id: int):
-    startTime = datetime.now()
+async def shufersal(ctx, *args):
+    product_id = int(args[0])
+    speed = 1.0
+    reverse = False
+    time = False
+
+    try:
+        arg_ptr = 0
+        while arg_ptr < len(args)-1:
+            arg_ptr += 1
+            arg = args[arg_ptr]
+
+            if arg == '-s':
+                arg_ptr += 1
+                arg = float(args[arg_ptr])
+                reverse = arg < 0
+                speed = abs(arg)
+            elif arg == '-r':
+                reverse = True
+            elif arg == '-t':
+                time = True
+            else:
+                await ctx.send("Wrong Arguments (remind me to write a usuage message)!")
+    except Exception as e:
+        await ctx.send("Wrong Arguments (remind me to write a usuage message)!")
+
+    if time:    
+        startTime = datetime.now()
 
     gif = shufer.get_gif(product_id)
 
     if gif:
-        await ctx.send(f"{(datetime.now() - startTime).total_seconds()}'s", file=discord.File(shufer.get_gif(product_id), f"{product_id}.gif"))
+        await ctx.send(f"{(datetime.now() - startTime).total_seconds()}'s" if time else None, file=discord.File(shufer.get_gif(product_id, fps=speed*10, reverse=reverse), f"{product_id}.gif"))
     else:
         await ctx.send("Error has occured!")
 
--- shufer.py
+++ shufer.py
@@ -1,11 +1,11 @@
 import io, imageio
 
 
-def get_gif(product_id: int):
+def get_gif(product_id: int, fps: int = 10, reverse: bool = False):
     gif_bytes = io.BytesIO()
 
-    with imageio.get_writer(gif_bytes, format='gif', mode='I') as writer:
-        for i in range(40):         
+    with imageio.get_writer(gif_bytes, format='gif', mode='I', fps=fps) as writer:
+        for i in range(40)[::1*(-1 if reverse else 1)]:         
             try:   
                 image = imageio.imread(f"https://media.shufersal.co.il/product_images/products_360/{product_id}/files/360_assets/index/images/{product_id}_{i+1}.jpg")
                 writer.append_data(image)
